4d6147b60653d90347ffbcf3614566da55d7133e
Taylor <taylorhcarroll1@gmail.com>
2020-03-19 11:46:50 -0700

    done

---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿--SELECT Title FROM Album
+--WHERE Label = 'Columbia'
+
+--SELECT Title, ArtistId FROM Album
+
+--SELECT al.Title, ar.ArtistName, g.[Label] FROM Album al
+--LEFT JOIN Artist ar ON ar.Id = al.ArtistId
+--LEFT JOIN Genre g ON g.Id = al.GenreId
+
+--SELECT al.Title, al.Id, ar.ArtistName, g.[Label] FROM Album al
+--LEFT JOIN Artist ar ON ar.Id = al.ArtistId
+--LEFT JOIN Genre g ON g.Id = al.GenreId                               
+--WHERE al.Id = 17
+
+--UPDATE Album
+--SET Title = 'Eliminator 2: Judgement Day'
+--WHERE Id = 17
+
+--SELECT * FROM Album
+--Where Id = 17
+
+DELETE FROM Album WHERE Id = 17
+
+SELECT Id, Title
+FROM Album
+
+INSERT INTO Song
+
+
+
+
